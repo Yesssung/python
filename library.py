5e5151a76cf238f9e41edd0620f43c8bd8eee5d4
Yesssung <cjsrnr0504@naver.com>
2025-01-16 23:52:44 +0900

    .

---
+++ library.py
@@ -0,0 +1,7 @@
+# 원하는 라이브러리 찾아서 가져다 쓰기만 하면 된다
+# 라이브러리 사용 전에는 "import" 꼭 해줘야 한다는거 잊지 말기
+
+# datetime.date : 연, 월, 일
+import datetime
+day1 = datetime.date(2021, 12, 14)
+day2 = datetime.date(2025, 4, 5)
