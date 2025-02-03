2df820e6b204df71027e66deaee451a90c992afb
Yesssung <cjsrnr0504@naver.com>
2025-02-04 00:38:11 +0900

    0204

--- practice.py
+++ practice.py
@@ -1,3 +1,4 @@
+# 1. 상속
 class Calculator:
     def __init__(self):
         self.value = 0
@@ -6,5 +7,12 @@ class Calculator:
         self.value += val
 
 class UpgradeCaldulator(Calculator):
-    def minus(self):
-        result = self.val
+    def minus(self, val):
+        self.value -= val
+
+
+# 2. 오버라이딩
+class MaxLimitCalulator(Calculator):
+    def add(self, val):
+        if self.val > 100:
+            self.val = 100
